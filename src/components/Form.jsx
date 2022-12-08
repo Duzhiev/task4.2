3344f9d9a10df54795f2cac1b86c5d0299aa605e
Duzhiev <magomed.duzhiev@gmail.com>
2022-12-08 14:22:12 +0300

    Поисковик

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
+import { shop } from '../shop'
+
 
 const Form = () => {
-    
+
     const [added, setAdded] = useState(false)
     const [info, setInfo] = useState(false)
-
+    const [arr, setArr] = useState(shop.challenges)
     const add = () => setAdded(true)
     const inf = () => setInfo(true)
 
@@ -14,30 +16,54 @@ const Form = () => {
     const addInf = () => {
         setInfo(false)
     }
-    
-    return (
-        <>
-            <div className='commonBlock'>
-                <div><img src="nike.png" alt="Кроссовок" /></div>
-                <div className='footer1'>
-                    <div className='name'>
-                        <h1>Nike Sneaker</h1>
-                        <h1>$120</h1>
+
+    const [value, setValue] = useState('');
+
+    const filteredName = (e) => {
+        setValue(e.target.value)
+        setArr(
+            shop.challenges.filter((item) => {
+                return item.name.toLowerCase().includes(e.target.value.toLowerCase())
+            })
+        )
+    }
+
+return (
+    <>
+        <input type='text'
+            placeholder='Search in the country...'
+            className='search_input'
+            value={value}
+            onChange={filteredName}
+             />
+        <div className='Block1'>
+            {arr.map((item, index) => {
+                return (
+                    <div className='commonBlock'>
+                        <div>{item.img}</div>
+                        <div className='footer1'>
+                            <div className='name'>
+                                <h1>{item.name}</h1>
+                                <h1>{item.price}</h1>
+                            </div>
+                            <button className='btn' onClick={inf}>Детали</button>
+                            <div className='addBlock'>{info ? <div className='addBlock1'>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                <button className='addBtn' onClick={addInf}>x</button>
+                            </div> : null}</div>
+                        </div>
+                        <div>
+                            <div className='text'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
+                            <button className='btn2' onClick={add}>{added ? 'В корзине' : 'Добавить в корзину'}</button>
+                        </div>
+                        <div onClick={addremove}>{added ? <button className='delete'>Удалить из корзины</button>
+                            : null}</div>
                     </div>
-                    <button className='btn' onClick={inf}>Детали</button>
-                    <div className='addBlock'>{info? <div className='addBlock1'>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        <button className='addBtn' onClick={addInf}>x</button>
-                    </div>: null}</div>
-                </div>
-                <div>
-                    <div className='text'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
-                    <button className='btn2' onClick={add}>{added? 'В корзине': 'Добавить в корзину'}</button>
-                </div>
-                <div  onClick={addremove}>{added? <button className='delete'>Удалить из корзины</button>
-                : null}</div>
-            </div>
-        </>
-    );
+                )
+            })}
+        </div>
+
+    </>
+);
 };
 
 export default Form;
---
+++ src/shop.js
@@ -0,0 +1,35 @@
+export const shop = {
+    challenges: [
+        {
+            img: <img src="nike.png" alt="Кроссовок" />,
+            name: 'Nike',
+            price: "$120"
+            
+        },
+        {
+            img: <img src = 'adidas.png' alt="Кроссовок"></img>,
+            name: 'Adidas',
+            price: '$200',
+        },
+        {
+            img:<img src = 'puma.png' alt = 'Кроссовок'></img>,
+            name: 'Puma',
+            price: '$56'
+        },
+        {
+            img: <img src="converse.png" alt="Кроссовок"></img>,
+            name: 'Converse',
+            price: '$1231'
+        },
+        {
+            img: <img src="saucony.png" alt="Кроссовок"></img>,
+            name: 'Saucony',
+            price: "$100"
+        },
+        {
+            img: <img src="koloshi.png" alt="Кроссовок"></img>,
+            name: 'koloshi',
+            price: "$500"
+        }
+    ]
+}
